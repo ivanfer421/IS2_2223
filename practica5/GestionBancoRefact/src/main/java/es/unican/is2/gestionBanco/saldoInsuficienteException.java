9cb1c0c6591d71c61d026453f1372fe2b985ca07
ivanfer421 <ifa908@alumnos.unican.es>
2023-04-27 12:29:55 +0200

    Metricas obtenidas codigo inicial

---
+++ practica5/GestionBancoRefact/src/main/java/es/unican/is2/gestionBanco/saldoInsuficienteException.java
@@ -0,0 +1,9 @@
+package es.unican.is2.gestionBanco;
+
+@SuppressWarnings("serial")
+public class saldoInsuficienteException extends RuntimeException {
+
+	public saldoInsuficienteException (String mensaje) {
+		super(mensaje);
+	}
+}
